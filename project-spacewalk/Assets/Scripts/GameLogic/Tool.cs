d664981af211bcd1ecc08cb9446df146f2b312ac
Rustam <rustam.alashrafov@gmail.com>
2020-01-31 22:53:52 +0100

    Grabbing Tools by mouse over and launching them on OnMouseExit

---
+++ project-spacewalk/Assets/Scripts/GameLogic/Tool.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace SpaceWalk.GameLogic
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Tool : MonoBehaviour
+    {
+        [SerializeField] private ToolType _toolType;
+        [SerializeField] private float _releaseForce;
+        private Vector3 _prevMousePos;
+        private Rigidbody2D _rigidBody;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnMouseExit()
+        {
+            var position = GetMousePositionInWorld();
+            var direction = (position - _prevMousePos);
+            _rigidBody.velocity = Vector3.zero;
+            _rigidBody.AddForce(direction * _releaseForce);
+
+            Debug.Log("Direction: " + direction);
+        }
+        private void OnMouseOver()
+        {
+            var position = GetMousePositionInWorld();
+            transform.localPosition = position;
+            _prevMousePos = position;
+        }
+
+        private Vector3 GetMousePositionInWorld()
+        {
+            var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0f;
+            return position;
+        }
+    }
+    public enum ToolType
+    {
+        WireDetector,
+        PipeDetector,
+        NailPistol
+    }
+}
