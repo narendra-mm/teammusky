84b6677a46bbd086ee4155eb3adfe3db19349954
Narendra Yenugula <narendra.yenugula@mediamonks.com>
2020-02-01 10:08:16 +0100

    Added Ducttape baker

---
+++ project-spacewalk/Assets/Scripts/TrailBaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailBaker : MonoBehaviour
+{
+	[SerializeField] private TrailRenderer _trail;
+	[SerializeField] private MeshFilter _meshFilter;
+	[SerializeField] private Camera _camera;
+	[SerializeField] private bool _keepTransforms;
+
+	[ContextMenu("BakeMesh")]
+	public void BakeTrailMesh()
+	{
+		_trail.BakeMesh(_meshFilter.mesh, _camera, _keepTransforms);
+		_trail.Clear();
+	}
+
+	public void ClearBakedMesh()
+	{
+		_meshFilter.mesh = null;
+	}
+}
