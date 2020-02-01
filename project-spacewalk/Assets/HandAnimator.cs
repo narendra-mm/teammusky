814ca00174bd98414fe8f8dc01dcf9461486ecba
Christian Maulany <cmaulany@gmail.com>
2020-02-01 19:24:47 +0100

    Fixed prefab setup, reskinned arm

---
+++ project-spacewalk/Assets/HandAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite handOpen;
+    [SerializeField] private Sprite handClosed;
+    private SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            renderer.sprite = handClosed;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            renderer.sprite = handOpen;
+        }
+    }
+}
