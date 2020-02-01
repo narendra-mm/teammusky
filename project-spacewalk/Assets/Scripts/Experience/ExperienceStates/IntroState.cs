dfb08baec806f2ba009816e0674f54f10ac8724f
Rustam <rustam.alashrafov@gmail.com>
2020-02-01 15:06:39 +0100

    Added TMPro

---
+++ project-spacewalk/Assets/Scripts/Experience/ExperienceStates/IntroState.cs
@@ -0,0 +1,33 @@
+﻿using SpaceWalk.GameLogic;
+using UnityEngine;
+
+namespace Experience.ExperienceState
+{
+	public class IntroState : MonoBehaviour, IExperienceState
+	{
+        public void InitializeState(ExperienceStateManager context)
+        {
+            Debug.Log($"Initialised {this.GetType()}");
+        }
+
+        public void EnterState()
+        {
+            Debug.Log($"Entered {this.GetType()}");
+        }
+
+        public void UpdateState()
+        {
+            
+        }
+
+        public void ExitState()
+        {
+            Debug.Log($"Exited {this.GetType()}");
+        }
+
+        public void DisposeState()
+		{
+			Debug.Log($"Disposed {this.GetType()}");
+        }
+	}
+}
