1590db473ff617445d98d0dbcf40d5fb3323461a
Rustam <rustam.alashrafov@gmail.com>
2020-02-01 22:57:10 +0100

    Added the ship  damage logic

---
+++ project-spacewalk/Assets/Scripts/GameLogic/ShipPart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace SpaceWalk.GameLogic
+{
+    [RequireComponent(typeof(PlayableDirector))]
+    public class ShipPart : MonoBehaviour
+    {
+        public ShipPartType ShipPartType;
+
+        private PlayableDirector _director;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _director = GetComponent<PlayableDirector>();
+        }
+
+        public void PlayTimeline()
+        {
+            _director.Play();
+        }
+        public void ResetTimeLine()
+        {
+            _director.Stop();
+        }
+    }
+}
