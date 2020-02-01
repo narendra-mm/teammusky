8941dace10122bb21661f05ee8b89ac89f63c7c7
Christian Maulany <cmaulany@gmail.com>
2020-02-01 11:54:14 +0100

    Added spots to tools

---
+++ project-spacewalk/Assets/LightFader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFader : MonoBehaviour
+{
+    private Light light;
+
+    public float changeDuration = 1f;
+    public float targetIntensity;
+
+    private float lastIntensity;
+    private float lastIntensityChange;
+
+    public void SetTargetIntensity(float intensity) {
+        Debug.Log("Setting intensity " + intensity);
+        lastIntensity = light.intensity;
+        lastIntensityChange = Time.time;
+        targetIntensity = intensity;
+    }
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.time - lastIntensityChange;
+        light.intensity = Mathf.SmoothStep(lastIntensity, targetIntensity, deltaTime / changeDuration);
+    }
+}
