da64629424dd82e07ffd50918226ddfc94babbeb
Rustam <rustam.alashrafov@gmail.com>
2020-02-01 16:12:48 +0100

    Added LightControlTimeline

---
+++ project-spacewalk/Assets/Scripts/Timeline/LightControl/LightControlTrack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[TrackClipType(typeof(LightControlAsset))]
+[TrackBindingType(typeof(Light))]
+public class LightControlTrack : TrackAsset
+{
+    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+    {
+        return ScriptPlayable<LightControlMixerBehaviour>.Create(graph, inputCount);
+    }
+}
